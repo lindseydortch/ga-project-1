f31e792f3b10e2ddb2b2d4208730c4d8eb812071
lindseydortch <codewithlinds@gmail.com>
2021-05-06 20:37:42 -0500

    adding functionality to be able to select only two cards at a time

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // BRING IN CARD 1
 let cards = document.querySelectorAll('.card')
+let cardFront = document.querySelector('.card-front')
+
+// for (let i = 0; i < cardFront.length; i++) {
+//   console.log(cardFront[i])
+// }
 
 for(let i = 0; i < cards.length; i++) {
   cards[i].addEventListener('click', (e) => {
-    cards[i].classList.toggle('card-hover')
+    let cardHover = cards[i].classList.toggle('card-hover')
+    //let value = 0 
+
+    console.log(cardHover)
+    if (cardHover === true) {
+      console.log('Card has been hovered')
+    } 
   })
 }
 
---
+++ notes.md
@@ -0,0 +1,4 @@
+1. Need to be able to only flip two cards at a time 
+  - If one of the cards is selected, then you can select one more
+  - If two cards are selected, then you can't select any more cards
+2. Need to be able to see if the content on the card matches
